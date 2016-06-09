306efc3c603e5df94644dd77d9047cf00ad85e32
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-09 12:04:56 -0500

    Controlador Vista Login.
    Se instanciaron con Butterknife los elementos de la vista login, y me implementaron a nivel básico el comportamiento de los botones de la vista.

--- app/src/main/java/com/edx/shell/android/androidchat/LoginActivity.java
+++ app/src/main/java/com/edx/shell/android/androidchat/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.edx.shell.android.androidchat;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @Bind(R.id.til_email)
+    TextInputLayout tilEmail;
+    @Bind(R.id.til_pass)
+    TextInputLayout tilPass;
+    @Bind(R.id.rel_main_container)
+    RelativeLayout relMainContainer;
+    @Bind(R.id.btn_signin)
+    Button btnSignin;
+    @Bind(R.id.btn_signup)
+    Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_signin)
+    public void onClickBtnSignin() {
+        String email = "";
+        if (tilEmail.getEditText() != null) {
+            email = tilEmail.getEditText().getText().toString().trim();
+        }
+        Log.d("AndroidChat", email);
+    }
+
+    @OnClick(R.id.btn_signin)
+    public void onClickBtnSignup() {
     }
 }
