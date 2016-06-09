02689c8fdd59c7a6db7f15cc3def26f8d01b071d
Cesar Morales Garduño <cesar.uam.computacion@gmail.com>
2016-06-09 14:57:31 -0500

    Controlador Firebase.
    Se crearon funciones y métodos de ayuda de la clase FirebaseHelper.

--- app/src/main/java/com/edx/shell/android/androidchat/FirebaseHelper.java
+++ app/src/main/java/com/edx/shell/android/androidchat/FirebaseHelper.java
@@ -1,10 +1,13 @@
 package com.edx.shell.android.androidchat;
 
 import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class FirebaseHelper {
 
@@ -12,7 +15,7 @@ public class FirebaseHelper {
     private static final String FIREBASE_URL = "https://shell-android-chat.firebaseio.com/";
     private static final String USERS_PATH = "users";
     private static final String CONTACTS_PATH = "contacts";
-    private static final String CHAT_PATH = "chats";
+    private static final String CHATS_PATH = "chats";
     private static final String SEPARATOR = "___";
 
     // VARIABLES
@@ -28,6 +31,7 @@ public class FirebaseHelper {
 
     /**
      * Función que devuelve el correo electrónico del usuario logueado.
+     *
      * @return
      */
     public String getAuthUserEmail() {
@@ -47,5 +51,132 @@ public class FirebaseHelper {
 
     private static class SingletonHolder {
         private static final FirebaseHelper INSTANCE = new FirebaseHelper();
+    }
+
+    /**
+     * Accede a la referencia del usuario
+     * @param email
+     * @return
+     */
+    public Firebase getUserReference(String email) {
+        Firebase reference = null;
+        if (email != null) {
+            String emailKey = email.replace(".", "_");
+            reference = dataReference.getRoot()
+                    .child(USERS_PATH)
+                    .child(emailKey);
+        }
+        return reference;
+    }
+
+    /**
+     * Accede a la referencia de usuario logueado
+     * @return
+     */
+    public Firebase getMyUserReference() {
+        return getUserReference(getAuthUserEmail());
+    }
+
+    /**
+     * Obtiene todos los contactos de la referencia
+     * @param email
+     * @return
+     */
+    public Firebase getContactsReference(String email) {
+        return getUserReference(email).child(CONTACTS_PATH);
+    }
+
+    /**
+     * Obtiene todos los contactos del usuario logueado
+     * @return
+     */
+    public Firebase getMyContactsReference(){
+        return getContactsReference(getAuthUserEmail());
+    }
+
+    /**
+     * Devuelve una referencia de acuerdo al contacto.
+     * @param email
+     * @param childEmail
+     * @return
+     */
+    public Firebase getOneContactReference(String email, String childEmail) {
+        String childKey = childEmail.replace(".", "_");
+        return getUserReference(email).child(CONTACTS_PATH)
+                .child(childKey);
+    }
+
+    /**
+     * Función para obtener la referencia del chat del usuario
+     * @param receiver
+     * @return
+     */
+    public Firebase getChatReferences(String receiver) {
+        String keySender = getAuthUserEmail().replace(".", "_");
+        String keyReceiver = receiver.replace(".", "_");
+
+        String keyChat = keySender + SEPARATOR + keyReceiver;
+        if (keySender.compareTo(keyReceiver) > 0) {
+            keyChat = keyReceiver + SEPARATOR + keySender;
+        }
+
+        return dataReference.getRoot()
+                .child(CHATS_PATH)
+                .child(keyChat);
+    }
+
+    /**
+     * Cambiar el estatus de la conexión de un usuario
+     * @param online
+     */
+    public void changeUserConnectionStatus(boolean online) {
+        if (getMyUserReference() != null) {
+            Map<String, Object> updates = new HashMap<>();
+            updates.put("online", online);
+            getMyUserReference().updateChildren(updates);
+            notifyContactsOfConnectionChange(online);
+        }
+    }
+
+    /**
+     * Notificar a los contactos que el estado ha sido cambiado
+     * @param online
+     */
+    public void notifyContactsOfConnectionChange(boolean online) {
+        notifyContactsOfConnectionChange(online, false);
+    }
+
+    /**
+     * Función para cerrar sesión
+     */
+    public void signoff() {
+        notifyContactsOfConnectionChange(false, true);
+    }
+
+    /**
+     * Función para notificar el estado del usuario y cierre de sesión
+     * @param online
+     * @param signoff
+     */
+    private void notifyContactsOfConnectionChange(final boolean online, final boolean signoff) {
+        final String myEmail = getAuthUserEmail();
+        getMyUserReference().addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    String email = child.getKey();
+                    Firebase reference = getOneContactReference(email, myEmail);
+                    reference.setValue(online);
+                }
+                if (signoff) {
+                    dataReference.unauth();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
