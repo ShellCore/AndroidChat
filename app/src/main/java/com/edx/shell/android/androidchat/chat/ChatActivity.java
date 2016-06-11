2ce285d2e5bfaf3d65999d861d1c47c1980dbe86
Cesar Morales Garduño <shell.one.core@gmail.com>
2016-06-11 11:05:37 -0500

    Interfaz Chat con contacto.
    Se creó la interfaz necesaria para la vista de chat.

---
+++ app/src/main/java/com/edx/shell/android/androidchat/chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.edx.shell.android.androidchat.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.edx.shell.android.androidchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
