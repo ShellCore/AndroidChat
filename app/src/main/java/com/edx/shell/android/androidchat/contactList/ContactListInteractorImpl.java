3b838b2ffe9a8e8f0b3089a6d4646246fd3fe535
Cesar Morales Garduño <shell.one.core@gmail.com>
2016-06-10 22:22:09 -0500

    Implementación de los interactuadores de la lista de contactos.
    - Se creo la implementación de los iteractuadores de la lista de contactos.
    - Se creo la clase de implementación del repositorio.

--- app/src/main/java/com/edx/shell/android/androidchat/contactList/ContactListInteractorImpl.java
+++ app/src/main/java/com/edx/shell/android/androidchat/contactList/ContactListInteractorImpl.java
@@ -1,23 +1,30 @@
 package com.edx.shell.android.androidchat.contactList;
 
 public class ContactListInteractorImpl implements ContactListInteractor {
+
+    ContactListRepository repository;
+
+    public ContactListInteractorImpl() {
+        repository = new ContactListRepositoryImpl();
+    }
+
     @Override
     public void subscribe() {
-
+        repository.subscribeToContactListEvents();
     }
 
     @Override
     public void unsubscribe() {
-
+        repository.unsubscribeToContactListEvent();
     }
 
     @Override
     public void destroyListener() {
-
+        repository.destroyListener();
     }
 
     @Override
     public void removeContact(String email) {
-
+        repository.removeContact(email);
     }
 }
---
+++ app/src/main/java/com/edx/shell/android/androidchat/contactList/ContactListRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.edx.shell.android.androidchat.contactList;
+
+public class ContactListRepositoryImpl implements ContactListRepository {
+    @Override
+    public void signOff() {
+
+    }
+
+    @Override
+    public String getCurrentUserEmail() {
+        return null;
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+
+    @Override
+    public void subscribeToContactListEvents() {
+
+    }
+
+    @Override
+    public void unsubscribeToContactListEvent() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
